aa5484c75edd289b10e0ccea272cc5624dcdd71b
johnny <johnny@172.16.100.13>
2015-09-14 09:39:27 -0500

    Car Class Implementation of Vehicle

--- src/abstractiondemo/Car.java
+++ src/abstractiondemo/Car.java
@@ -9,6 +9,76 @@ package abstractiondemo;
  *
  * @author johnny
  */
-public class Car {
+public class Car implements Vehicle{
+
+    private int year;
+    private int engineHP;
+    private String name;
+    private String manufacName;
+    private String engineSize;
+    private String color;
+    
+    
+    public Car() {
+    }
+
+    public Car(int year, int engineHP, String name, String manufacName, String engineSize, String color) {
+        this.year = year;
+        this.engineHP = engineHP;
+        this.name = name;
+        this.manufacName = manufacName;
+        this.engineSize = engineSize;
+        this.color = color;
+    }
+    @Override
+    public int getYear() {
+        return year;
+    }
+    @Override
+    public void setYear(int year) {
+        this.year = year;
+    }
+    @Override
+    public int getEngineHP() {
+        return engineHP;
+    }
+    @Override
+    public void setEngineHP(int engineHP) {
+        this.engineHP = engineHP;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getManufacName() {
+        return manufacName;
+    }
+    @Override
+    public void setManufacName(String manufacName) {
+        this.manufacName = manufacName;
+    }
+    @Override
+    public String getEngineSize() {
+        return engineSize;
+    }
+    @Override
+    public void setEngineSize(String engineSize) {
+        this.engineSize = engineSize;
+    }
+    @Override
+    public String getColor() {
+        return color;
+    }
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    
     
 }
--- src/abstractiondemo/Motorcycle.java
+++ src/abstractiondemo/Motorcycle.java
@@ -9,6 +9,69 @@ package abstractiondemo;
  *
  * @author johnny
  */
-public class Motorcycle {
+public class Motorcycle implements Vehicle{
+
+    public Motorcycle() {
+    }
+
+    @Override
+    public int getYear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setYear(int year) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getManufacName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setManufacName(String manufacName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEngineSize(String engineSize) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getEngineSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEngineHP(int engineHP) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getEngineHP() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setColor(String color) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getColor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
